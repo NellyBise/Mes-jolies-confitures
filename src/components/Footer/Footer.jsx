108adbe65d260b224ec0c11f8266f75f7b96a6a7
NellyBise <nellybise@free.fr>
2024-11-20 14:55:03 +0100

    Modify footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom'
 function Footer() {
   return (
     <footer className="footer">
+      <Link className="footer__text" to="https://www.nelly-bise.fr/">
+        Réalisé par Nelly Bise
+      </Link>
       <Link className="footer__text" to="/credits">
         Crédits des images
       </Link>
